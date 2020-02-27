b14a37a0dc178f6e001d663a1678f7dd039f6d67
James Vera <jdvera2015@gmail.com>
2020-02-26 19:26:13 -0600

    product-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `<div><h1>{{pageTitle}}</h1><p>I love her</p></div>`
+  template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>`
 })
 export class AppComponent {
   pageTitle: string = 'AshBebe is mah bebe';
