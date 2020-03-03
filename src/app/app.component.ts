3ce36c895120cc58fd42bb791491324039cfd7b1
James Vera <jdvera2015@gmail.com>
2020-03-02 20:06:43 -0600

    image appear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>`
 })
 export class AppComponent {
-  pageTitle: string = 'AshBebe is mah bebe';
+  pageTitle: string = 'Fantasy Costco';
 }
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
    pageTitle: string = "Product List";
+   imageWidth: number = 50;
+   imageMargin: number = 2;
+   showImage: boolean = false;
    products: any[] = [
       {
          "productId": 2,
@@ -28,4 +31,8 @@ export class ProductListComponent {
          "imageUrl": "assets/images/hammer.png"
        }
    ];
+
+   toggleImage(): void {
+      this.showImage = !this.showImage;
+   }
 }
