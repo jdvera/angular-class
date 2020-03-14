a36080c67c9d833c3d236927b7ac373562701f3e
James Vera <jdvera2015@gmail.com>
2020-03-14 18:06:21 -0500

    added interface and styled header

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
+
 
 @Component({
    selector: 'pm-products',
-   templateUrl: './product-list.component.html'
+   templateUrl: './product-list.component.html',
+   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
    pageTitle: string = "Product List";
@@ -10,7 +13,7 @@ export class ProductListComponent {
    imageMargin: number = 2;
    showImage: boolean = false;
    listFilter: string = "cart";
-   products: any[] = [
+   products: IProduct[] = [
       {
          "productId": 2,
          "productName": "Garden Cart",
