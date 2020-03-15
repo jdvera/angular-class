7ee70dab0009039d8a45f67111ab00abd9adf11a
James Vera <jdvera2015@gmail.com>
2020-03-14 19:06:35 -0500

    implemented stars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConverToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ConverToSpacesPipe
+    ConverToSpacesPipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
