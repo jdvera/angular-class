93ec4ee247d8cf63661fbb86d94a8bbf7f173eeb
James Vera <jdvera2015@gmail.com>
2020-03-15 14:47:51 -0500

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConverToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,20 @@ import { ProductDetailComponent } from './products/product-detail.component';
     ProductListComponent,
     ConverToSpacesPipe,
     StarComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "products/:id", component: ProductDetailComponent },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" }
+    ])
   ],
   bootstrap: [AppComponent]
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,7 +4,7 @@ import { ProductService } from './product.service';
 
 
 @Component({
-   selector: 'pm-products',
+   // selector: 'pm-products',
    templateUrl: './product-list.component.html',
    styleUrls: ['./product-list.component.css']
 })
